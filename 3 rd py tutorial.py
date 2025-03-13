7012bb3fd6e6418e1853b5e24f8508da943f40ac
sairamana1 <tsairamana101@gmail.com>
2025-03-13 21:06:14 +0530

    Add files via upload

---
+++ 3 rd py tutorial.py
@@ -0,0 +1,84 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+3
+5
+9_8
+98
+9-8
+1
+6*5
+30
+8/4
+2.0
+9/2
+4.5
+9/4
+2.25
+87/7
+12.428571428571429
+5//2
+2
+87//7
+12
+4+8-10+5
+7
+4-6-7
+-9
+5-2-
+SyntaxError: invalid syntax
+8+38*2
+84
+(8+38)*2
+92
+2^10
+8
+2*2*2
+8
+2**3
+8
+>>> 2**10
+1024
+>>> 10//3
+3
+>>> 10/3
+3.3333333333333335
+>>> 10%3
+1
+>>> 'sai'
+'sai'
+>>> print('sai')
+sai
+>>> print('sai's laptop')
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("sai's laptop")
+...       
+sai's laptop
+>>> print("sai"laptop"")
+...       
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print('sai "laptop"')
+...       
+sai "laptop"
+>>> print('sai's "laptop"')
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print('sai\'s "laptop")
+...       
+SyntaxError: unterminated string literal (detected at line 1); perhaps you escaped the end quote?
+>>> print('sai\'s "laptop"')
+...       
+sai's "laptop"
+>>> print(10*'sai')
+...       
+saisaisaisaisaisaisaisaisaisai
+>>> print(10*'"SAI"')
+...       
+"SAI""SAI""SAI""SAI""SAI""SAI""SAI""SAI""SAI""SAI"
+>>> print('d:sai\ndl')
+...       
+d:sai
+dl
+>>> print(r'd:sai\ndl')
+...       
+d:sai\ndl
